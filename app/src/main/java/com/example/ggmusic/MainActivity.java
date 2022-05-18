540a7a1acaf95135f4cdac8bdee3da2a56a286c2
jesee030 <desiasd@qq,comgit config --global user.email desiasd@qq.com>
2022-05-18 16:13:35 +0800

    音乐播放器一

---
+++ app/src/main/java/com/example/ggmusic/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.ggmusic;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private ContentResolver mContentResolver;
+    private ListView mPlaylist;
+    private MediaCursorAdapter mCursorAdapter;
+
+    private final String SELECTION =
+            MediaStore.Audio.Media.IS_MUSIC + " = ? " + " AND " +
+                    MediaStore.Audio.Media.MIME_TYPE + " LIKE ? ";
+    private final String[] SELECTION_ARGS = {
+            Integer.toString(1),
+            "audio/mpeg"
+    };
+    private final int REQUEST_EXTERNAL_STORAGE = 1;
+
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        mContentResolver = getContentResolver();
+        mCursorAdapter = new MediaCursorAdapter(MainActivity.this);
+        mPlaylist=findViewById(R.id.lv_playlist);
+        mPlaylist.setAdapter(mCursorAdapter);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                requestPermissions(PERMISSIONS_STORAGE,
+                        REQUEST_EXTERNAL_STORAGE);
+            }
+        } else {
+            initPlaylist();
+        }
+
+
+    }
+
+    private void initPlaylist() {
+        Cursor mCursor = mContentResolver.query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                null,
+                SELECTION,
+                SELECTION_ARGS,
+                MediaStore.Audio.Media.DEFAULT_SORT_ORDER
+        );
+
+        mCursorAdapter.swapCursor(mCursor);
+        mCursorAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initPlaylist();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
